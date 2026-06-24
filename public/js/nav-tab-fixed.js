c39d61c1a6955eceb07f9ac187e599fa530eacd5
angel <angelito09866@gmail.com>
2026-06-24 00:23:56 -0600

    Agrega secciones de GPS y Archivos con transiciones

--- public/js/nav-tab-fixed.js
+++ public/js/nav-tab-fixed.js
@@ -8,31 +8,25 @@ export class NavTabFixed extends HTMLElement {
  }
 
  connectedCallback() {
-  this.classList.add("md-tab", "fixed")
+this.classList.add("md-tab", "fixed");
 
   if (!this.creado) {
-
-   this.innerHTML = /* HTML */`
-    <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
-     <span class="material-symbols-outlined">home</span>
-     Inicio
-    </a>
- 
-    <a ${resaltaSiEstasEn(["/select.html"])} href="select.html">
-     <span class="material-symbols-outlined">bottom_panel_close</span>
-     Select
-    </a>
- 
-    <a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
-     <span class="material-symbols-outlined">help</span>
-     Ayuda
-    </a>`
-
-   this.creado = true
-
+    this.innerHTML = /* HTML */`
+      <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+        <span class="material-symbols-outlined">home</span>Inicio
+      </a>
+      <a ${resaltaSiEstasEn(["/archivos.html"])} href="archivos.html">
+        <span class="material-symbols-outlined">add_photo_alternate</span>Archivos
+      </a>
+      <a ${resaltaSiEstasEn(["/gps.html"])} href="gps.html">
+        <span class="material-symbols-outlined">location_on</span>Ubicación
+      </a>
+      <a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
+        <span class="material-symbols-outlined">help</span>Ayuda
+      </a>`;
+    this.creado = true;
   }
-
- }
+}
 
 }
 
